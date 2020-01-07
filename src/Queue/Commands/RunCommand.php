69399b89c06be984dff2b5303a42e99ce9707781
Vitaliy <theyamshikov@yandex.ru>
2020-01-07 21:48:43 +0600

    feat: add option channel

--- src/Queue/Commands/RunCommand.php
+++ src/Queue/Commands/RunCommand.php
@@ -4,6 +4,7 @@ namespace PhpLab\Sandbox\Queue\Commands;
 
 use PhpLab\Sandbox\Queue\Domain\Interfaces\JobServiceInterface;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -19,17 +20,32 @@ class RunCommand extends Command
         $this->jobService = $jobService;
     }
 
+    protected function configure()
+    {
+        $this->addArgument('channel', InputArgument::OPTIONAL);
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln(['<fg=white># Queue run</>']);
-        $output->writeln(['']);
-        $total = $this->jobService->runAll();
-        if ($total) {
-            $output->writeln(['<fg=green>Complete ' . $total . ' jobs!</>']);
+        $output->writeln('<fg=white># Queue run</>');
+        $output->writeln('');
+
+        $channel = $input->getArgument('channel');
+        if($channel) {
+            $output->writeln("Channel: <fg=blue>{$channel}</>");
         } else {
-            $output->writeln(['<fg=green>Jobs empty!</>']);
+            $output->writeln("Channel: <fg=blue>all</>");
         }
-        $output->writeln(['']);
+
+        $output->writeln('');
+
+        $total = $this->jobService->runAll($channel);
+        if ($total) {
+            $output->writeln('<fg=green>Complete ' . $total . ' jobs!</>');
+        } else {
+            $output->writeln('<fg=magenta>Jobs empty!</>');
+        }
+        $output->writeln('');
     }
 
 }
