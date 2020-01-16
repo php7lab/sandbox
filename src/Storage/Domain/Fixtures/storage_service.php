28a27b8191240e1ddbcf306e3d1ca3ebf13baa6e
Vitaliy <vitaliy_krg@rocketfirm.com>
2020-01-16 13:43:59 +0600

    update fixture

--- src/Storage/Domain/Fixtures/storage_service.php
+++ src/Storage/Domain/Fixtures/storage_service.php
@@ -10,4 +10,13 @@ return [
 		'created_at' => '2020-01-16 12:58:00',
 		'updated_at' => null,
 	],
+	[
+		'id' => 2,
+		'title' => 'Временная папка пользователя',
+		'name' => 'user_temp',
+		'path' => 'user/temp',
+		'status' => 1,
+		'created_at' => '2020-01-16 12:58:00',
+		'updated_at' => null,
+	],
 ];
