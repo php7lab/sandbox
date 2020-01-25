f83207cdd6bd0ed2a4e7a09dd8b8230dc0b83c0d
Vitaliy <theyamshikov@yandex.ru>
2020-01-25 18:25:46 +0600

    feat: make modal widget

---
+++ src/Web/Twig/ModalExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace PhpLab\Sandbox\Web\Twig;
+
+use PhpLab\Sandbox\Web\Widgets\ModalWidget;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class ModalExtension extends AbstractExtension
+{
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('modal', [$this, 'modal'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function modal(string $tagId, string $header, string $body, string $footer)
+    {
+        $widgetInstance = new ModalWidget;
+        $widgetInstance->tagId = $tagId;
+        $widgetInstance->header = $header;
+        $widgetInstance->body = $body;
+        $widgetInstance->footer = $footer;
+        return $widgetInstance->render();
+    }
+
+}
