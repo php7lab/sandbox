ddc1072e4ecd8ce875eaeed91128184b3c9b1a13
Vitaliy <theyamshikov@yandex.ru>
2020-02-01 20:47:37 +0600

    clean

--- src/Bot/Api/Controllers/ChatController.php
+++ src/Bot/Api/Controllers/ChatController.php
@@ -72,7 +72,6 @@ class ChatController
     {
         $content = file_get_contents('C:\Users\Admin\Desktop\answer_databse\answer_databse\answer_databse.txt');
         $content = str_replace("\r\n", PHP_EOL, $content);
-//$lineArray = explode(PHP_EOL, $content);
         $lineArray = \PhpLab\Sandbox\Common\Helpers\StringHelper::textToLines($content);
         $rrrrr = [];
 
@@ -91,110 +90,10 @@ class ChatController
             $line = \PhpLab\Sandbox\Bot\Domain\Helpers\MlHelper::prepareWord($line);
         }
         $rrr = array_unique($rrr);
-//dd($rrr);
 
         dd(array_slice($rrr, 0, 200));
 
         return array_slice($lineArray, 0, 20);
-    }
-
-    private function dialog(WordClassificator $wordClassificator, InputInterface $input, OutputInterface $output)
-    {
-        /*$question = new Question('> ', '');
-        $userAnwer = $this->getHelper('question')->ask($input, $output, $question);*/
-        $userAnwer = 'как дел';
-        $predict = $wordClassificator->predict($userAnwer);
-        $output->writeln($predict);
-    }
-
-    private function train(WordClassificator $wordClassificator, KNearestNeighbors $classifier): KNearestNeighbors
-    {
-        $wordCollection = include(__DIR__ . '/../Resources/data/bot_words.php');
-        $arr = $wordClassificator->generateTrain($wordCollection, 2);
-        list($samples, $labels) = $wordClassificator->prepareSamplesForTraining($arr);
-        $classifier->train($samples, $labels);
-        return $classifier;
-    }
-
-    private function test(WordClassificator $wordClassificator)
-    {
-        $tests = [
-            [
-                'sample' => 'как дела',
-                'expected' => 'как дела',
-            ],
-            [
-                'sample' => 'как дел',
-                'expected' => 'как дела',
-            ],
-            [
-                'sample' => 'как дила',
-                'expected' => 'как дела',
-            ],
-            [
-                'sample' => 'каг дила',
-                'expected' => 'как дела',
-            ],
-            [
-                'sample' => 'каг дилы',
-                'expected' => 'как дела',
-            ],
-
-            [
-                'sample' => 'привет',
-                'expected' => 'привет',
-            ],
-            [
-                'sample' => 'превет',
-                'expected' => 'привет',
-            ],
-            [
-                'sample' => 'превед',
-                'expected' => 'привет',
-            ],
-            [
-                'sample' => 'привед',
-                'expected' => 'привет',
-            ],
-            /*[
-                'sample' => 'првет',
-                'expected' => 'привет',
-            ],*/
-
-            [
-                'sample' => 'да',
-                'expected' => 'да',
-            ],
-            [
-                'sample' => 'дп',
-                'expected' => 'да',
-            ],
-
-            [
-                'sample' => 'нет',
-                'expected' => 'нет',
-            ],
-            [
-                'sample' => 'нпт',
-                'expected' => 'нет',
-            ],
-            [
-                'sample' => 'нее',
-                'expected' => 'нет',
-            ],
-
-        ];
-
-        $testResults = [];
-        foreach ($tests as $test) {
-            $predict = $wordClassificator->predict($test['sample']);
-            $isOk = $predict == $test['expected'];
-            $testResults[] = $isOk;
-            if ( ! $isOk) {
-                dump("{$test['sample']} - {$test['expected']} - {$predict}");
-            }
-        }
-        dd($testResults);
     }
 
 }
