2e38b14d017302a1b3dda768bfda5fbed44aa2ba
Vitaliy <theyamshikov@yandex.ru>
2020-02-02 19:31:44 +0600

    refacto bin

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -1,5 +1,47 @@
 <?php
 
+use Symfony\Component\Console\Application;
+
+/**
+ * @var Application $application
+ */
+
+// --- Bot ---
+
+use PhpLab\Sandbox\Bot\Commands\BotCommand;
+
+$command = new BotCommand;
+$application->add($command);
+
+// --- Stress ---
+
+use PhpLab\Sandbox\Dev\Commands\StressCommand;
+use PhpLab\Sandbox\Dev\Domain\Services\StressService;
+
+// создаем и объявляем команды
+$stressService = new StressService;
+$command = new StressCommand(null, $stressService);
+$application->add($command);
+
+// --- Generator ---
+
+use PhpLab\Sandbox\Generator\Commands\DomainCommand;
+use PhpLab\Sandbox\Generator\Commands\ModuleCommand;
+use PhpLab\Sandbox\Generator\Domain\Services\DomainService;
+use PhpLab\Sandbox\Generator\Domain\Services\ModuleService;
+
+// создаем и объявляем команды
+$domainService = new DomainService;
+$moduleService = new ModuleService;
+
+$command = new DomainCommand(null, $domainService);
+$application->add($command);
+
+$command = new ModuleCommand(null, $moduleService);
+$application->add($command);
+
+// --- Package ---
+
 use PhpLab\Sandbox\Package\Commands\GitChangedCommand;
 use PhpLab\Sandbox\Package\Commands\GitNeedReleaseCommand;
 use PhpLab\Sandbox\Package\Commands\GitPullCommand;
@@ -9,13 +51,8 @@ use PhpLab\Sandbox\Package\Domain\Repositories\File\GroupRepository;
 use PhpLab\Sandbox\Package\Domain\Repositories\File\PackageRepository;
 use PhpLab\Sandbox\Package\Domain\Services\GitService;
 use PhpLab\Sandbox\Package\Domain\Services\PackageService;
-use Symfony\Component\Console\Application;
 
-/**
- * @var Application $application
- */
-
-$fileName = __DIR__ . '/../../../Package/Domain/Data/package_group.php';
+$fileName = __DIR__ . '/../src/Package/Domain/Data/package_group.php';
 $groupRepository = new GroupRepository($fileName);
 $packageRepository = new PackageRepository($groupRepository);
 $gitRepository = new GitRepository($packageRepository);
--- src/bin/modules/bot/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use PhpLab\Sandbox\Bot\Commands\BotCommand;
-/**
- * @var Application $application
- */
-
-$command = new BotCommand(null, $domainService);
-$application->add($command);
