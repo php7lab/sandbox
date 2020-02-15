5fcc9897a5784a7755f5cbbc0357e79f839bcf08
Vitaliy <theyamshikov@yandex.ru>
2020-02-16 01:04:56 +0600

    refactor

--- src/Dashboard/Domain/Services/DocService.php
+++ src/Dashboard/Domain/Services/DocService.php
@@ -11,20 +11,42 @@ class DocService implements DocServiceInterface
 {
 
     private $docFileNameMask;
+    private $docDirectory;
 
-    public function __construct(string $docFileNameMask = 'docs/api/dist/v{version}.html')
+    public function __construct(string $docFileNameMask = 'v{version}.html', string $docDirectory = 'docs/api/dist')
     {
         $this->docFileNameMask = $docFileNameMask;
+        $this->docDirectory = $docDirectory;
     }
 
-    public function htmlByVersion($version) {
+    public function versionList(): array {
+        $list = FileHelper::scanDir(FileHelper::rootPath() . '/' . $this->docDirectory);
+        $pattern = $this->generateRegExp($this->docFileNameMask);
+        $result = [];
+        foreach ($list as $fileName) {
+            if(preg_match($pattern, $fileName, $matches)) {
+                $version = $matches[1];
+                $result[] = $version;
+            }
+        }
+        return $result;
+    }
+
+    public function htmlByVersion(int $version): string {
         $fileName = StringHelper::renderTemplate($this->docFileNameMask, ['version' => $version]);
-        $docFileName = FileHelper::path($fileName);
+        $docFileName = FileHelper::path($this->docDirectory . '/' . $fileName);
         $htmlContent = @file_get_contents($docFileName);
         if(empty($htmlContent)) {
             throw new NotFoundException("Not found API documentation for version v{$version}!");
         }
         return $htmlContent;
+    }
+
+    private function generateRegExp($docFileNameMask) {
+        $pattern = preg_quote($docFileNameMask);
+        $pattern = str_replace('\{version\}', '(\d+)', $pattern);
+        $pattern = '/' . $pattern . '/i';
+        return $pattern;
     }
 
 }
