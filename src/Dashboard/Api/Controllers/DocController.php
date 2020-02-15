2f03a3b184bc0299e0d804f8212deab5f9342280
Vitaliy <theyamshikov@yandex.ru>
2020-02-16 01:55:08 +0600

    feat: move dashboard api

---
+++ src/Dashboard/Api/Controllers/DocController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace PhpLab\Sandbox\Dashboard\Api\Controllers;
+
+use PhpLab\Core\Exceptions\NotFoundException;
+use PhpLab\Sandbox\Dashboard\Domain\Interfaces\Services\DocServiceInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class DocController extends AbstractController
+{
+
+    private $docService;
+
+    public function __construct(DocServiceInterface $docService)
+    {
+        $this->docService = $docService;
+    }
+
+    public function index()
+    {
+        $versionList = $this->docService->versionList();
+        $html = '<ul>';
+        foreach ($versionList as $version) {
+            $html .= "<li><a href=\"/api/v$version\">API version $version</a></li>";
+        }
+        $html .= '</ul>';
+        return new Response($html);
+    }
+
+    public function show($version)
+    {
+        try {
+            $htmlContent = $this->docService->htmlByVersion($version);
+        } catch (NotFoundException $e) {
+            throw new NotFoundHttpException("Not found API documentation for version v{$version}!");
+        }
+        return new Response($htmlContent);
+    }
+
+}
---
+++ src/Dashboard/DashboardBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PhpLab\Sandbox\Dashboard;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DashboardBundle extends Bundle
+{
+
+}
