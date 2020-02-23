2836c3c459ec5fd1df6a418af19e9500f619b291
Vitaliy <theyamshikov@yandex.ru>
2020-02-23 14:30:12 +0600

    refactor: test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,4 @@
 <?php
 
 require __DIR__ . '/../../../autoload.php';
-$_ENV['APP_ENV'] = 'test';
-PhpLab\Core\Libs\Env\DotEnvHelper::init();
+\PhpLab\Core\Libs\Env\DotEnvHelper::init();
--- tests/config/bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require __DIR__ . '/../../../../autoload.php';
-\PhpLab\Core\Libs\Env\DotEnvHelper::init();
