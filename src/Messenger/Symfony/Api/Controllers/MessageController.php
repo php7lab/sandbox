7b9c8f5f7b6c0e41e0ff3d49b36b1972e7459736
Vitaliy <theyamshikov@yandex.ru>
2020-07-20 00:10:02 +0600

    upd

--- src/Messenger/Symfony/Api/Controllers/MessageController.php
+++ src/Messenger/Symfony/Api/Controllers/MessageController.php
@@ -2,7 +2,13 @@
 
 namespace PhpLab\Sandbox\Messenger\Symfony\Api\Controllers;
 
+use PhpBundle\User\Domain\Symfony\Authenticator;
+use PhpBundle\User\Domain\Traits\AccessTrait;
+use PhpLab\Core\Domain\Entities\Query\Where;
 use PhpLab\Core\Domain\Exceptions\UnprocessibleEntityException;
+use PhpLab\Core\Domain\Helpers\EntityHelper;
+use PhpLab\Core\Domain\Helpers\QueryHelper;
+use PhpLab\Core\Domain\Libs\DataProvider;
 use PhpLab\Rest\Base\BaseCrudApiController;
 use PhpLab\Rest\Libs\Serializer\JsonRestSerializer;
 use PhpLab\Sandbox\Messenger\Domain\Interfaces\ChatServiceInterface;
@@ -13,12 +19,52 @@ use Symfony\Component\HttpFoundation\Response;
 
 class MessageController extends BaseCrudApiController
 {
+    
+    use AccessTrait;
 
-    public function __construct(MessageServiceInterface $chatService)
+    private $authenticator;
+    private $chatService;
+
+    public function __construct(ChatServiceInterface $chatService, MessageServiceInterface $messageService, Authenticator $authenticator)
     {
-        $this->service = $chatService;
+        $this->service = $messageService;
+        $this->authenticator = $authenticator;
+        $this->chatService = $chatService;
+        $this->checkAuth();
     }
     
+    public function allByChatId(Request $request, int $chatId = null) {
+        //dd($chatId);
+        $chatEntity = $this->chatService->oneById($chatId);
+        $chatCollection = $this->chatService->all();
+        $queryParams = $request->query->all();
+        unset($queryParams['Authorization']);
+        $query = QueryHelper::getAllParams($queryParams);
+        $whereChat = new Where('chat_id', $chatId);
+        $query->whereNew($whereChat);
+        $query->orderBy(['id'=>SORT_ASC]);
+        $page = $request->get("page", 1);
+        $pageSize = $request->get("per-page", 10000000000);
+        // todo: make converter
+        $dataProvider = new DataProvider($this->service, $query, $page, $pageSize);
+        $dataProvider->getEntity()->setMaxPageSize(10000000000);
+
+        $response = new JsonResponse;
+
+
+        $serializer = new JsonRestSerializer($response);
+        $serializer->serializeDataProviderEntity($dataProvider->getAll());
+        return $response;
+
+        //return new JsonResponse($dataProvider);
+
+        /*return $this->render('@Messenger/message/index.list.html.twig', [
+            'dataProviderEntity' => $dataProvider->getAll(),
+            'chatEntity' => $chatEntity,
+            'chatCollection' => $chatCollection,
+        ]);*/
+    }
+
     public function sendMessageFromBot(Request $request, string $bot) {
         $response = new JsonResponse;
         $serializer = new JsonRestSerializer($response);
